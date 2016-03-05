203097020a4352b7f56cc46f29a588556974a3c8
Ardian Ajvazi <ardian_46@hotmail.com>
2016-03-05 12:19:23 -0800

    authrouter, server and gulpfile

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = module.exports = exports = express();
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/Space-Shooter');
+var port = 3000;
+
+const authRouter = require(__dirname + '/routes/auth_routes');
+const publicRouter = require(__dirname + '/routes/public_routes');
+
+module.exports.server = app.listen(port, () => console.log('Server running ' + port));//eslint-disable-line
