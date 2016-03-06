226558a8294bb6b3bcfeb9ab9bf203273ebecc7e
Ardian Ajvazi <ardian_46@hotmail.com>
2016-03-06 13:12:28 -0800

    services and signin controller

--- client/app/js/auth/controllers/signup_controller.js
+++ client/app/js/auth/controllers/signup_controller.js
@@ -1,1 +1,11 @@
-signup_controller.js
+module.exports = function(app) {
+  app.controller('SignupController', ['$scope', '$location', 'userAuth', function($scope, $location, auth) {
+    $scope.signup = true;
+    $scope.submit = function(user) {
+      auth.createUser(user, function() {
+        $scope.updateUsername();
+        $location.path('/dashboard');
+      });
+    };
+  }]);
+};
---
+++ client/app/js/auth/services/user_auth.js
@@ -0,0 +1,70 @@
+module.exports = function(app) {
+  app.factory('userAuth', ['$http', '$window', function($http, $window) {
+    var token;
+    var user;
+    var auth = {
+      createUser: function(user, callback) {
+        callback = callback || function() {};
+        $http.post('http://localhost:3000/api/signup', user)
+        .then(function(res) {
+          token = $window.localStorage.token = res.data.token;
+          callback(null);
+        }, function(res) {
+          callback(res.err)
+        });
+      },
+
+      signIn: function(user, callback) {
+        callback = callback || function() {};
+        $http({
+          method: 'GET',
+          url: 'http://localhost:3000/api/signin',
+          headers: {
+            'Authorization': 'Basic ' + btoa((user.email + ':' + user.password))
+          }
+        })
+        .then(function(res) {
+          token = $window.localStorage.token = res.data.token;
+          callback(null);
+        }, function(res) {
+          callback(res);
+        });
+      },
+
+      getToken: function() {
+        token = token || $window.localStorage.token;
+        return token;
+      },
+
+      signOut: function(callback) {
+        $window.localStorage.token = null;
+        token = null;
+        user = null;
+        if(callback) callback();
+      },
+
+      getUsername: function(callback) {
+        callback callback || function() {};
+        $http({
+          method: 'GET',
+          url: 'http://localhost:3000/api/currentuser',
+          headers: {
+            token: auth.getToken()
+          }
+        })
+        .then(function(res) {
+          user = res.data.username;
+          callback(res);
+        }, function(res) {
+          callback(res);
+        });
+      },
+
+      username: function() {
+        if(!user) auth.getUsername();
+        return user;
+      }
+    };
+    return auth;
+  }]);
+};
