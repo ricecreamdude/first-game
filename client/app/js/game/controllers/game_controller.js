01b2d7e2217fba1544630b71cd1c6b54322cf557
jessethach <jessethach@gmail.com>
2016-03-07 15:09:12 -0800

    developed modal for pause menu

--- client/app/js/game/controllers/game_controller.js
+++ client/app/js/game/controllers/game_controller.js
@@ -1,7 +1,24 @@
 var angular = require('angular');
 
 module.exports = function(app) {
-  app.controller('GameController', ['$scope', '$http', 'cfResource', function() {
-    
+  app.controller('GameController', ['$scope', 'modals', function($scope, modals) {
+
+    $scope.alertSomething = function() {
+      var promise = modals.open(
+        "alert",
+        {
+          message: "I think you are kind of beautiful!"
+        }
+      );
+      promise.then(
+				function handleResolve( response ) {
+					console.log( "Alert resolved." );
+				},
+				function handleReject( error ) {
+					console.warn( "Alert rejected!" );
+				}
+			);
+    }
+
   }]);
 };
