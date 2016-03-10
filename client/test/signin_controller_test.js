2e535af7598caa113a3722c88e6db72bf2f314e6
jessethach <jessethach@gmail.com>
2016-03-10 10:41:56 -0800

    added tests for controllers

---
+++ client/test/signin_controller_test.js
@@ -0,0 +1,60 @@
+var angular = require('angular');
+
+describe('Signin controller', () => {
+  var $httpBackend;
+  var $scope;
+  var $ControllerConstructor;
+  var user;
+
+  beforeEach(angular.mock.module('gameApp'));
+
+  beforeEach(angular.mock.inject(($rootScope, $controller) => {
+    $ControllerConstructor = $controller;
+    $scope = $rootScope.$new();
+  }));
+
+  it('should be able to make a signin controller', () => {
+    var signinController = $ControllerConstructor('SigninController', {$scope});
+    expect(typeof signinController).toBe('object');
+
+    expect(typeof $scope.submit).toBe('function');
+  });
+
+  describe('REST requests', function() {
+    beforeEach(angular.mock.inject(function(_$httpBackend_) {
+      $httpBackend = _$httpBackend_;
+      $ControllerConstructor('SigninController', {$scope});
+    }));
+
+    afterEach(() => {
+      $httpBackend.verifyNoOutstandingExpectation();
+      $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it('should make a get request to /api/signin and receive an error', () => {
+      var user = {
+        email: 'test123@test.com'
+      };
+
+      $httpBackend.expectGET('http://localhost:3000/api/signin').respond(401, user);
+      $scope.submit(user);
+      $httpBackend.flush();
+      expect($scope.userWrong).toBe(true);
+    });
+
+    it('should make a get request to /api/signin', () => {
+      var user = {
+        username: 'test123',
+        email: 'test123@test.com',
+        password: 'foobar123',
+        token: 'testtoken'
+      };
+
+      $httpBackend.expectGET('http://localhost:3000/api/signin').respond(200, user);
+      $scope.submit(user);
+      $httpBackend.flush();
+      expect($scope.userWrong).toBe(null);
+    });
+  });
+
+});
--- client/test/test_entry.js
+++ client/test/test_entry.js
@@ -2,3 +2,5 @@ require(__dirname + '/../app/js/entry');
 require('angular-mocks');
 
 require(__dirname + '/style_controller_test.js');
+require(__dirname + '/signin_controller_test.js');
+require(__dirname + '/signup_controller_test.js');
